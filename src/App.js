88d56105a557b0cf7556bae25c8496d96178b954
davidwarrington <davidmwarrington+github@gmail.com>
2018-02-12 23:21:17 +0000

    Boolean API Tests

--- src/App.js
+++ src/App.js
@@ -5,6 +5,42 @@ import './App.css';
 import UsernameInput from './Components/Homepage/UsernameInput/UsernameInput.jsx';
 import Header from './Components/Header/Header.jsx';
 
+let results;
+
+// Use Fetch API for getting info from GitHub API
+fetch('https://api.github.com/users/davidwarrington')
+  .then(function(response) {
+    return response.json()
+  }).then(function(json) {
+    results = json;
+    console.log(results)
+    questions.forEach(question => {
+      question.status = testQuery({ key: question.key });
+      console.log(question.status);
+    });
+  }).catch(function(ex) {
+    console.log('parsing failed', ex)
+  });
+
+const testQuery = opts => results[opts.key] ? true : false;
+
+const questions = [
+  {
+    "query": "has_name",
+    "type": "boolean",
+    "key": "name",
+    "status": false
+  },
+  {
+    "query": "has_email",
+    "type": "boolean",
+    "key": "email",
+    "status": false
+  }
+];
+
+// console.log(questions[0].status());
+
 const Home = () => (
   <UsernameInput />
 );
