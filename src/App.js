1753fe7f02199e4859a429f9f20850671b3d4777
davidwarrington <davidmwarrington+github@gmail.com>
2018-02-22 21:53:16 +0000

    Request API info on submit

--- src/App.js
+++ src/App.js
@@ -10,33 +10,30 @@ import Header from './Components/Header/Header.jsx';
 // Import functions for handling data in back-end
 import api_req from './Functions/api';
 
-let username = 'davidwarrington';
-api_req(username);
+// let user_data;
 
-let user_data;
+// const testQuery = opts => user_data[opts.key] ? 'complete' : 'incomplete';
 
-const testQuery = opts => user_data[opts.key] ? 'complete' : 'incomplete';
-
-const questions = [
-  {
-    "query": "has_name",
-    "type": "boolean",
-    "key": "name",
-    "status": false
-  },
-  {
-    "query": "has_email",
-    "type": "boolean",
-    "key": "email",
-    "status": false
-  }
-];
+// const questions = [
+//   {
+//     "query": "has_name",
+//     "type": "boolean",
+//     "key": "name",
+//     "status": false
+//   },
+//   {
+//     "query": "has_email",
+//     "type": "boolean",
+//     "key": "email",
+//     "status": false
+//   }
+// ];
 
 // console.log(questions[0].status());
 
 const TaskView = (props) => (
   <Header 
-    username={props.match.params.username} />
+    username={props.username} />
 );
 
 class App extends Component {
@@ -44,7 +41,8 @@ class App extends Component {
     super(props);
 
     this.state = {
-      username: ''
+      username: '',
+      user_info: null
     };
 
     this.usernameChangeHandler = this.usernameChangeHandler.bind(this);
@@ -52,20 +50,35 @@ class App extends Component {
   }
 
   usernameChangeHandler = (event) => {
-    this.setState({username: event.target.value});
+    this.setState({ username: event.target.value });
   }
 
   usernameSubmitHandler = (event) => {
     event.preventDefault();
-    console.log(event);
-    // this.props.router.push();
+    this.apiHandler();
+  }
+
+  apiHandler = () => {
+    // Use Fetch API for getting info from GitHub API
+    fetch(`https://api.github.com/users/${this.state.username}`)
+      .then(response => response.json())
+      .then(json => {
+        json.message === "Not Found" ? console.error('Uh Oh') : this.setState({ user_info: json });
+
+          // questions.forEach(question => {
+          //     question.status = testQuery({ key: question.key });
+          //     console.log(question.status);
+          // });
+      })
+      .catch(function(ex) {
+          console.log('parsing failed', ex)
+      });
   }
 
   render() {
     return (
       <Router>
         <div className="App">
-            <button onClick={this.usernameHandler}>{this.state.username}</button>
             <Route 
               exact 
               path="/" 
@@ -73,10 +86,27 @@ class App extends Component {
                               {...this.state}
                               change={this.usernameChangeHandler}
                               submit={this.usernameSubmitHandler}
-                            />}
+                            />
+                     }
             />
-            <Route exact path="/:username" component={TaskView}/>
-            <Route exact path="/:username/all" component={TaskView}/>
+            <Route 
+              exact 
+              path="/:username" 
+              render={({ match }) => <TaskView
+                                      {...this.state}
+                                      username={match.params.username}
+                                     />
+                     }
+            />
+            <Route 
+              exact 
+              path="/:username/all" 
+              render={({ match }) => <TaskView
+                                      {...this.state}
+                                      username={match.params.username}
+                                     />
+                     }
+            />
         </div>
       </Router>
     );
