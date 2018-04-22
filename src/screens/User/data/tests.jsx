290c7aaf71080a055f74bf830bc734d63bf765d1
davidwarrington <davidmwarrington+github@gmail.com>
2018-04-22 17:45:01 +0100

    Removed unnecessary keys from test objects

--- src/screens/User/data/tests.jsx
+++ src/screens/User/data/tests.jsx
@@ -21,12 +21,9 @@ import React from 'react';
  */
 const tests = [
     { 
-        id: 'has_name', 
-        api_key: 'name', 
-        status: false,
-        ignored: false, 
-        type: 'boolean', 
-        answer: null, 
+        id: 'has_name',
+        api_key: 'name',
+        type: 'boolean',
         fail_message: <p>You have no name set. Change this to let an employer know who you are.</p>, 
         pass_message: function() { return `Congrats! Employers can see that you are ${this.answer}.` }, 
         title: 'Name'
@@ -34,10 +31,7 @@ const tests = [
     { 
         id: 'has_bio', 
         api_key: 'bio', 
-        status: false, 
-        ignored: false, 
         type: 'boolean', 
-        answer: null, 
         fail_message: <p>Write a brief bio to give potential employers an idea of what makes you tick.</p>, 
         pass_message: function() { return `You have a bio! Hopefully this leaves employers with a good impression of you.` }, 
         title: 'Bio'
@@ -45,10 +39,7 @@ const tests = [
     { 
         id: 'has_email', 
         api_key: 'email', 
-        status: false, 
-        ignored: false, 
         type: 'boolean', 
-        answer: null, 
         fail_message: <p>You have no email address available. Change this to let employers contact you.</p>, 
         // pass_message: val => <p>Congrats! Employers can contact you here: <a href={`mailto:${val}`}>{val}</a>.</p> 
         pass_message: function() { return `Congrats! Employers can contact you here: ${this.answer}.` }, 
@@ -57,10 +48,7 @@ const tests = [
     { 
         id: 'has_website', 
         api_key: 'blog', 
-        status: false, 
-        ignored: false, 
         type: 'boolean', 
-        answer: null, 
         fail_message: <p>Employers might prefer to look at a portfolio website, you should share a link to yours via the profile settings.</p>,
         // pass_message: val => <p>Employers can see your work here: <a href={val}>{val}</a>.</p> 
         pass_message: function() { return `Employers can see your work here: ${this.answer}` }, 
@@ -69,10 +57,7 @@ const tests = [
     { 
         id: 'has_location', 
         api_key: 'location', 
-        status: false, 
-        ignored: false, 
         type: 'boolean', 
-        answer: null, 
         fail_message: <p>There is no location associated with your account. Doing so will let employers know if they're looking at somebody local</p>,
         pass_message: function() { return `Employers in the region of ${this.answer} may be more keen to contact you.` }, 
         title: 'Location'
@@ -80,10 +65,7 @@ const tests = [
     { 
         id: 'is_hireable', 
         api_key: 'hireable', 
-        status: false, 
-        ignored: false, 
         type: 'boolean', 
-        answer: null, 
         fail_message: <p>You're profile says you're not hireable. An employer might not choose to ignore that.</p>,
         pass_message: function() { return `You're letting people know that you are hireable.` }, 
         title: 'Hireable'
@@ -91,10 +73,7 @@ const tests = [
     { 
         id: 'num_repos', 
         api_key: 'public_repos', 
-        status: false, 
-        ignored: false, 
         type: 'range', 
-        answer: null, 
         min_val: 1, 
         max_val: 4, 
         fail_message: <p>You have no public code repositories. Change this to let employers see what you can do!</p>, 
@@ -106,10 +85,7 @@ const tests = [
         id: 'is_active',
         api_key: 'created_at',
         array: 'events',
-        status: false,
-        ignored: false, 
         type: 'date_range',
-        answer: null,
         min_days: 7,
         max_days: 21,
         max_events: 8,
